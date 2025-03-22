c82a158a59935d4109815dc8e210c5f652b37c2f
Harit Sombatsiri <haritsombatsiri@gmail.com>
2025-03-23 00:17:33 +0700

    UPDATE : repository export

--- app/server/repository.ts
+++ app/server/repository.ts
@@ -1,7 +1,10 @@
 import AdoptionAPI from "./repositories/adoptionRepository"
 import ImageAPI from "./repositories/imageRepository"
+import OrderAPI from "./repositories/orderRepository"
 import PetAPI from "./repositories/petRepository"
+import ProductCategoryAPI from "./repositories/productCategoryRepository"
+import ProductAPI from "./repositories/productRepository"
 import UserAPI from "./repositories/userRepository"
 
 
-export { ImageAPI, UserAPI, PetAPI, AdoptionAPI }
+export { ImageAPI, UserAPI, PetAPI, AdoptionAPI, OrderAPI, ProductAPI, ProductCategoryAPI }
