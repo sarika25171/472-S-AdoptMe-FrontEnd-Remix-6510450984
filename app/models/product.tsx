22a5a3629e47913d93fc2492be95dcab82438c1c
Kriwn <nonisgod@gmail.com>
2025-03-24 15:27:43 +0700

    feat(Product):create model

---
+++ app/models/product.tsx
@@ -0,0 +1,14 @@
+export type ProductStatus = 'AVAILABLE' | 'OUT_OF_STOCK';
+
+export default interface Product {
+  id: number;
+  product_category_id: number;
+  name: string;
+  imageurl: string;
+  description: string;
+  price: number;
+  stock: number;
+  status: ProductStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
